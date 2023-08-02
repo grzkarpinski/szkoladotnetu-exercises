5353a56463ea6778ebab451752c1fb103fccd547
grzkarpinski <66385734+grzkarpinski@users.noreply.github.com>
2023-08-02 15:42:38 +0200

    Week 2 exercises

---
+++ week2/zadania_petle/Program.cs
@@ -0,0 +1,237 @@
+﻿// Napisz program, który sprawdzi ile jest liczb pierwszych w zakresie 0 – 100
+
+using System.Dynamic;
+
+int primeNumbers = 0;
+
+for  (int i = 2; i<= 100; i++) 
+{
+    int dividers = 0;
+
+    for (int j = 1; j <= i; j++) 
+    {
+        if (i % j == 0) 
+        {
+            dividers++;
+        }
+    }
+    if (dividers <= 2) 
+    {
+        primeNumbers++;
+        Console.WriteLine("Znalazłem liczbę pierwszą: " + i);
+    }
+}
+Console.WriteLine("\nZnaleziono " +  primeNumbers + " liczb pierwszych.");
+
+// Napisz program, w którym za pomocą pętli do…while znajdziesz wszystkie liczby parzyste z
+// zakresu 0 – 1000
+
+int number = 0;
+Console.WriteLine("\nLiczby parzyste:");
+do
+{
+    Console.Write($"{number}, ");
+    number += 2;
+} while (number < 1001);
+
+// Napisz program, który zaimplementuje ciąg Fibonacciego i wyświetli go na ekranie.
+int fMinusOne = 1;
+int fMinusTwo = 0;
+int nextFib = 0;
+
+Console.Write("\n\nCiag Fibonacciego:" + fMinusTwo + ", " + fMinusOne + ", ");
+
+while (nextFib <= 100) 
+{
+    nextFib = fMinusOne + fMinusTwo;
+    Console.Write(nextFib + ", ");
+    fMinusTwo = fMinusOne;
+    fMinusOne = nextFib;
+}
+
+/* Napisz program, który po podaniu liczby całkowitej wyświetli piramidę liczb od 1 do podanej
+liczby w formie jak poniżej:
+1
+2 3
+4 5 6
+7 8 9 10 */
+
+Console.WriteLine("\n\nPodaj liczbę do wygenerowania piramidy:");
+string pyramid_number = Console.ReadLine();
+int pyramid_number_int;
+Int32.TryParse(pyramid_number, out pyramid_number_int);
+Console.WriteLine();
+
+int pyr_number = 1;
+int counter = 1;
+while (counter <= pyramid_number_int) 
+{
+    for (int i = 1; i <= pyr_number; i++) 
+    {
+        if (counter <= pyramid_number_int) 
+        {
+            Console.Write($"{counter} ");
+            counter++;
+        }
+    }
+    Console.WriteLine();
+    pyr_number++;
+}
+
+// Napisz program, który dla liczb od 1 do 20 wyświetli na ekranie ich 3 potęgę.
+
+Console.WriteLine("\nPotęgi ^3 dla liczb od 1 do 20:\n");
+
+for (int i = 1; i <= 20; i++)
+{
+    int cube = i * i * i;
+    Console.WriteLine($"{i}^3 = {cube}");
+}
+
+// Napisz program, który dla liczb od 0 do 20 obliczy sumę wg wzoru:
+// 1 + 1/2 + 1/3 + 1/4 itd.
+
+Console.WriteLine("\nSumy ułamkowe 1 + 1/2 + 1/3 + 1/4 itd. :\n");
+
+for (double i = 0; i <=20.0; i++) 
+{
+    double sum = 0;
+    for (double j = 1.0; j <= i; j++) 
+    {
+        sum = sum + 1 / j;
+    }
+    Console.WriteLine($"Suma ułamkowa dla {i} = {Math.Round(sum, 3)}");
+}
+
+/* Napisz program, który dla liczby zadanej przez użytkownika narysuje diament o krótszej
+przekątnej o długości wprowadzonej przez użytkownika i wg wzoru:
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    * 
+*/
+
+Console.WriteLine("Ile pięter diamentu: ");
+string diamond = Console.ReadLine();
+int diamond_num;
+Int32.TryParse((diamond), out diamond_num);
+
+int astrix_num = 1;
+int max_astrix = diamond_num * 2 - 1;
+
+for (int i = 1; i <= diamond_num; i++) 
+{
+    int space_num = (max_astrix - astrix_num) / 2;
+    for (int j = 1; j <= space_num; j++) 
+    {
+        Console.Write(" ");
+    }
+    for (int k = 1; k <= astrix_num; k++) 
+    {
+        Console.Write("*");
+    }
+    for (int j = 1; j <= space_num; j++)
+    {
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+    astrix_num += 2;
+}
+// zmniejszanie diamentu
+astrix_num -= 2;
+
+for (int i = 1; i <= diamond_num; i++)
+{
+    astrix_num -= 2;
+    int space_num = (max_astrix - astrix_num) / 2;
+    for (int j = 1; j <= space_num; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 1; k <= astrix_num; k++)
+    {
+        Console.Write("*");
+    }
+    for (int j = 1; j <= space_num; j++)
+    {
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+
+/* Napisz program, który odwróci wprowadzony przez użytkownika ciąg znaków. Np.
+Testowe dane:
+Abcdefg
+Rezultat
+Gfedcba
+*/
+
+Console.Write("Wpisz słowo do odwrócenia: ");
+string word =  Console.ReadLine();
+
+string reversed_word = "";
+for (int letter = word.Length - 1; letter >= 0; letter--) 
+{
+    reversed_word += word[letter];
+}
+Console.WriteLine($"Twoje odwrócone słowo to: {reversed_word}");
+
+// Napisz program, który zamieni liczbę dziesiętną na liczbę binarną
+
+Console.Write("Wpisz liczbę do przeliczenia na system dwójkowy: ");
+string decimal_num = Console.ReadLine();
+int decimal_num_int;
+Int32.TryParse(decimal_num, out decimal_num_int);
+
+
+string num_string_rev = "";
+while  (decimal_num_int > 0) 
+{
+    int num_rest = decimal_num_int % 2;
+    num_string_rev += num_rest;
+    decimal_num_int /= 2;
+}
+
+// odwrócenie stringa
+
+string decimal_number = "";
+for (int letter = num_string_rev.Length - 1; letter >= 0; letter--)
+{
+    decimal_number += num_string_rev[letter];
+}
+Console.WriteLine($"W postaci binarnej to: {decimal_number}");
+
+
+// Napisz program, który znajdzie najmniejszą wspólną wielokrotność dla zadanych 2 liczb
+
+Console.Write("Liczymy najmniejszą wspólną wielokrotność. Wpisz 1 licznę: ");
+string number_one = Console.ReadLine();
+int number_one_int;
+Int32.TryParse(number_one,out number_one_int);
+
+Console.Write("Wpisz 2 licznę: ");
+string number_two = Console.ReadLine();
+int number_two_int;
+Int32.TryParse(number_two, out number_two_int);
+
+// 1. największy wspólny dzielink
+int nwd = 1;
+
+for (int i=2; i < number_one_int; i++) 
+{
+    if (number_one_int % i == 0 && number_two_int % i == 0) 
+    {
+        nwd = i;
+    }
+}
+
+// 2. nww
+
+int nww = number_one_int * number_two_int / nwd;
+
+Console.WriteLine($"Najmniejsza wspólna wielokrotność {number_one_int} i {number_two_int} to {nww}");
---
+++ week2/zadania_warunki/Program.cs
@@ -0,0 +1,387 @@
+﻿/* Napisz program w C#, który stworzy dwie zmienne int i sprawdzi czy są
+one równe czy nie.
+Dane testowe:
+a : 5
+b : 5
+Rezultat w terminalu :
+5 i 5 są równe */
+
+using System;
+using System.Reflection;
+
+int a = 5;
+int b = 5;
+
+if (a == b)
+{
+    Console.WriteLine($"{a} i {b} są równe");
+}
+else
+{
+    Console.WriteLine($"{a} i {b} NIE są równe");
+}
+
+/* Napisz program w C#, który sprawdzi czy podana przez użytkownika liczba
+jest parzysta czy nieparzysta.
+Dane testowe : 15
+Rezultat w terminalu :
+15 jest liczbą nieparzystą */
+
+Console.WriteLine("Wpisz liczbę do sprawdzenia:");
+string numberInput = Console.ReadLine();
+int number_int;
+int.TryParse( numberInput, out number_int);
+
+if (number_int % 2 == 0) 
+{
+    Console.WriteLine("Ta liczba jest parzysta!");
+}
+else 
+{
+    Console.WriteLine("Ta liczba NIE jest parzysta!");
+}
+
+/* Napisz program w C#, który sprawdzi czy podana przez użytkownika liczba
+jest dodatnia czy ujemna.
+Dane testowe : 14
+Rezultat w terminalu :
+14 jest liczbą dodatnią */
+
+Console.WriteLine("Wpisz liczbę do sprawdzenia czy jest dodatnia:");
+string numberInput1 = Console.ReadLine();
+int number_int1;
+int.TryParse(numberInput1, out number_int1);
+
+if (number_int1 > 0) 
+{
+    Console.WriteLine("Ta liczba jest większa od 0!");
+}
+else if (number_int1 < 0)
+{
+    Console.WriteLine("Ta liczba jest mniejsza od 0!");
+}
+else
+{
+    Console.WriteLine("Ta liczba to 0!");
+}
+
+/* Napisz program w C#, który sprawdzi czy podany przez użytkownika rok
+jest rokiem przestępnym.
+Dane testowe : 2016
+Rezultat w terminalu :
+2016 jest rokiem przestępnym */
+
+Console.WriteLine("Wpisz rok do sprawdzenia czy jest przestępny:");
+string yearInput = Console.ReadLine();
+int yearInt;
+int.TryParse( yearInput, out yearInt);
+
+if ((yearInt % 4 == 0 && yearInt % 100 != 0) || yearInt % 400 == 0) 
+{
+    Console.WriteLine("Ten rok jest przestępny!");
+}
+else
+{
+    Console.WriteLine("Ten rok nie jest przestępny!");
+}
+
+/* Napisz program w C#, który sprawdzi czy podany przez użytkownika wiek
+uprawnia go do ubiegania się o stanowisko posła, premiera, sentarora,
+prezydenta.
+Dane testowe : 21
+Rezultat w terminalu :
+Możesz zostać posłem */
+
+Console.WriteLine("Wpisz swój wiek:");
+string ageInput = Console.ReadLine();
+int ageInt;
+int.TryParse(ageInput, out ageInt);
+
+switch (ageInt) 
+{
+    case >= 35: Console.WriteLine("Możesz zostać posłem, premierem, senatorem i nawet prezydentem");
+    break;
+
+    case >= 30 : Console.WriteLine("Możesz zostać posłem, premierem lub senatorem");
+    break;
+
+    case >= 21: Console.WriteLine("Możesz zostać posłem lub premierem");
+    break;
+
+    default: Console.WriteLine("Nie masz biernego prawa wyborczego...");
+    break;
+}
+
+/* Napisz program w C#, który pobierze wzrost użytkownika i przypisze mu
+wymyśloną kategorię wzrostu.
+Dane testowe : 140
+Rezultat w terminalu :
+Jesteś krasnoludem */
+
+Console.WriteLine("Wpisz wzrost istoty:");
+string highInput = Console.ReadLine();
+int highInt;
+int.TryParse(highInput, out highInt);
+
+switch (highInt)
+{
+    case >= 190:
+        Console.WriteLine("To musi być troll!");
+        break;
+
+    case >= 160:
+        Console.WriteLine("To pewnie jest człowiek...");
+        break;
+
+    case >= 100:
+        Console.WriteLine("Raczej krasnolud.");
+        break;
+
+    default:
+        Console.WriteLine("Podstępny mały gnom!");
+        break;
+}
+
+/* Napisz program w C#, który pobierze 3 liczby od użytkownika i sprawdzi,
+która jest największa
+Dane testowe:
+25
+63
+79
+Rezultat w terminalu :
+79 jest największa z podanych */
+
+Console.WriteLine("Wpisz 1 liczbę:");
+string firstInput = Console.ReadLine();
+int firstInt;
+int.TryParse(firstInput, out firstInt);
+
+Console.WriteLine("Wpisz 2 liczbę:");
+string secondInput = Console.ReadLine();
+int secondInt;
+int.TryParse(secondInput, out secondInt);
+
+Console.WriteLine("Wpisz 3 liczbę:");
+string thirdInput = Console.ReadLine();
+int thirdInt;
+int.TryParse(thirdInput, out thirdInt);
+
+int biggestNumber = firstInt;
+
+if (secondInt > biggestNumber) 
+{
+    biggestNumber = secondInt;
+}
+else if (thirdInt > biggestNumber) 
+{
+    biggestNumber = thirdInt;
+}
+
+Console.WriteLine($"Największa z tych liczb to {biggestNumber}");
+
+/* Napisz program, który sprawdzi, czy kandydat może ubiegać się o miejsce
+na studiach wg. Następujących kryteriów:
+Wynik z Matury z matematyki powyżej 70
+Wynik z fizyki powyżej 55
+Wynik z chemii powyżej 45
+Łączny wynik z 3 przedmiotów powyżej 180
+Albo
+Wynik z matematyki i jednego przedmiotu powyżej 150
+Dane testowe:
+Matematyka 80
+Fizyka 71
+Chemia 0
+Rezultat w terminalu :
+Kandydat dopuszczony do rekrutacji */
+
+int math = 80;
+int physics = 71;
+int chemistry = 50;
+
+if ((math > 70 && physics > 55 && chemistry > 45 && math + physics + chemistry > 180) || math + physics > 150 || math + chemistry > 150) 
+{
+    Console.WriteLine("Kandydat dopuszczony do rekrutacji");
+}
+else 
+{
+    Console.WriteLine("Kandydat nie dopuszczony");
+}
+
+/* Napisz program, który odczyta temperaturę I zwróci nazwę jak w
+poniższych kryteriach
+Temp < 0 – cholernie piździ
+Temp 0 – 10 – zimno
+Temp 10 – 20 – chłodno
+Temp 20 – 30 – w sam raz
+Temp 30 – 40 – zaczyna być słabo, bo gorącoTemp >= 40 – a weź wyprowadzam się na Alaskę. */
+
+Console.WriteLine("Jaka jest temperatura?:");
+string tempInput = Console.ReadLine();
+int temp;
+int.TryParse(highInput, out temp);
+
+if (temp < 0)
+{
+    Console.WriteLine("Cholernie piździ");
+}
+else if (0 > temp && temp <= 10)
+{
+    Console.WriteLine("zimno");
+}
+else if (10 > temp && temp <= 20)
+{
+    Console.WriteLine("chłodno");
+}
+else if (20 > temp && temp <= 30)
+{
+    Console.WriteLine("W sam raz");
+}
+else if (30 > temp && temp <= 40)
+{
+    Console.WriteLine("zaczyna być słabo, bo gorąco");
+}
+else 
+{
+    Console.WriteLine("a weź wyprowadzam się na Alaskę.");
+}
+
+/* Napisz program, który sprawdzi, czy z 3 podanych długości można
+stworzyć trójkąt
+Dane testowe : 40 55 65
+Rezultat w terminalu :
+Można zbudować trójkąt */
+
+int bokA = 40;
+int bokB = 55;
+int bokC = 65;
+
+if (bokA + bokB > bokC || bokA + bokC > bokB || bokB + bokC > bokA)
+{
+    Console.WriteLine("Można zbudować trójkąt");
+}
+else 
+{
+    Console.WriteLine("Nie można zbudować trójkąta.");
+}
+
+/* Napisz program, który zmieni ocenę ucznia na jej opis wg podanej tabeli:
+Ocena Opis
+6 Celujący
+5 Bardzo dobry
+4 Dobry
+3 Dostateczny
+2 Dopuszczający
+1 Niedostateczny */
+
+int mark = 4;
+
+switch (mark) 
+{
+    case 1: Console.WriteLine("Niedostateczny");
+    break;
+
+    case 2: Console.WriteLine("Dopuszczający");
+        break;
+
+    case 3: Console.WriteLine("Dostateczny");
+        break;
+
+    case 4: Console.WriteLine("Dobry");
+        break;
+
+    case 5: Console.WriteLine("Bardzo dobry");
+        break;
+
+    case 6: Console.WriteLine("Celujący");
+        break;
+
+    default: Console.WriteLine("Nie ma takiej oceny");
+    break;
+}
+
+/* Napisz program, który pobierze numer dnia tygodnia i wyświetli jego
+nazwę
+Dane testowe : 4 Rezultat w terminalu :
+Czwartek */
+
+Console.WriteLine("Jaki jest dzień tygodnia? (numer)");
+string day  = Console.ReadLine();
+switch (day) 
+{
+    case "1": Console.WriteLine("Poniedziałek");
+    break;
+
+    case "2": Console.WriteLine("Wtorek");
+    break;
+
+    case "3": Console.WriteLine("Środa");
+    break;
+
+    case "4": Console.WriteLine("Czwartek");
+    break;
+
+    case "5": Console.WriteLine("Piątek");
+    break;
+
+    case "6": Console.WriteLine("Sobota");
+    break;
+
+    case "7": Console.WriteLine("Niedziela");
+    break;
+}
+
+/* Napisz program, który będzie posiadał proste menu (wg. Wzoru poniżej) I
+będzie prostym kalkulatorem
+Podaj pierwszą liczbę:
+…
+Podaj drugą liczbę:
+…
+Podaj numer operacji do wykonania:
+1. Dodawanie
+2. Odejmowanie
+3. Mnożenie
+4. Dzielenie
+…
+Twój wynik to: */
+
+Console.WriteLine("Podaj numer operacji do wykonania:");
+Console.WriteLine(@"1. Dodawanie
+2. Odejmowanie
+3. Mnożenie
+4. Dzielenie");
+
+string operation  = Console.ReadLine();
+
+Console.WriteLine("Wpisz pierwszą liczbę:");
+string number1 =  Console.ReadLine();
+double number1_int;
+Double.TryParse(number1, out number1_int);
+
+Console.WriteLine("Wpisz drugą liczbę:");
+string number2 = Console.ReadLine();
+double number2_int;
+Double.TryParse(number2, out number2_int);
+
+double output;
+switch (operation) 
+{
+    case "1":
+        output = number1_int + number2_int;
+        break;
+    case "2":
+        output = number1_int - number2_int;
+        break;
+    case "3":
+        output = number1_int * number2_int;
+        break;
+    case "4":
+        output = number1_int / number2_int;
+        break;
+    default:
+        output = 0;
+        Console.WriteLine("Nie znam takiej operacji");
+        break;
+}
+
+Console.WriteLine($"Wynik operacji  = {output}");
+
